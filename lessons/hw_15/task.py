3cf9837bf7f4be574b23d0cb3c2d4d276f8217b4
SEkaterina <katerina-kozyrenko@yandex.ru>
2023-08-06 15:43:07 +0300

    add_hw_15

--- lessons/hw_15/task.py
+++ lessons/hw_15/task.py
@@ -1,7 +1,6 @@
 # Возьмите любые 1-3 задачи из прошлых домашних заданий.
-# Добавьте к ним логирование ошибок и полезной
-# информации. Также реализуйте возможность запуска из
-# командной строки с передачей параметров.
+# Добавьте к ним логирование ошибок и полезной информации.
+# Также реализуйте возможность запуска из командной строки с передачей параметров.
 # Задача 2, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
 # Только здесь не только 16  представление
 
@@ -9,24 +8,31 @@
 import argparse
 import logging
 
+
 FORMAT = '{levelname} - {asctime} - {msg}'
-logging.basicConfig(level=logging.INFO, filename='my_func.log', encoding='utf-8',
+logging.basicConfig(level=logging.INFO, filename='func.log', encoding='utf-8',
                     format=FORMAT, style='{')
 logger = logging.getLogger(__name__)
 
 
-def get_number(number: int, mod: int = 10) -> float | str :
+def get_number(number: int, mod: int = 10) -> str:
+    """
+    Функция получает целое число, систему исчисления и возвращает его  строковое представление.
+    :param number: само число
+    :param mod: система исчисления
+    :return: строковое представление
+    """
     result = ''
     if mod < 0:
         logger.error(f'Система исчисления {mod} не существует')
-        return float('inf')
+        return 'error'
     while number != 0:
         try:
             result = str(number % mod) + result
             number //= mod
         except ZeroDivisionError as e:
             logger.error(f'Система исчисления {mod} не существует')
-            return float('inf')
+            return 'error'
     logger.info(f'{number}, {mod} = {result}')
     return result
 
@@ -34,17 +40,17 @@ def get_number(number: int, mod: int = 10) -> float | str :
 def parser_func():
     parser = argparse.ArgumentParser(description='Получаем аргументы из строки')
     parser.add_argument('--number')
-    parser.add_argument('--mod', default=10)
+    parser.add_argument('--mod', default=2)
     args = parser.parse_args()
     print(args)
     return get_number(int(args.number), int(args.mod))
 
 
 if __name__ == '__main__':
-    print(get_number(100, 0))
     print(get_number(100, 2))
-    print(get_number(100, 8))
-    parser_func()
+    print(get_number(100, 6))
+    print(get_number(100, 0))
 
-
-help(get_number())
+    # Реализация из командной строки
+    # print(parser_func())
+    # Пример написания в командной строке:   python Python\lessons\hw_15\task.py --number 500 --mod 2
