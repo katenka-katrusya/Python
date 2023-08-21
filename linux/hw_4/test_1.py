833379a8bda78e423aa7ce91699ccd82ba452770
SEkaterina <katerina-kozyrenko@yandex.ru>
2023-08-21 13:00:09 +0300

    upd_hw_4

--- linux/hw_4/test_1.py
+++ linux/hw_4/test_1.py
@@ -1,6 +1,7 @@
 from sshcheckers import ssh_checkout, upload_files
 import yaml
 
+
 class SSHTest:
     def __init__(self):
         with open('config.yaml') as f:
@@ -13,11 +14,14 @@ class SSHTest:
         res.append(ssh_checkout(self.data.get("ip"), self.data.get("user"), self.data.get("passwd"), "echo Connected to SSH"))
 
         # Загрузка файлов по SSH
-        res.append(upload_files(self.data.get("ip"), self.data.get("user"), self.data.get("passwd"), self.data.get("local_path"), self.data.get("remote_path")))
+        res.append(upload_files(self.data.get("ip"), self.data.get("user"), self.data.get("passwd"),
+                                self.data.get("local_path"), self.data.get("remote_path")))
 
         # Выполнение команды по SSH и проверка результатов
-        res.append(ssh_checkout(self.data.get("ip"), self.data.get("user"), self.data.get("passwd"), f"echo {self.data.get('passwd')} | sudo -S dpkg -i {self.data.get('remote_path')}", "Настраивается пакет"))
-        res.append(ssh_checkout(self.data.get("ip"), self.data.get(""), self.data.get("passwd"), f"echo {self.data.get('passwd')} | sudo -S dpkg -s p7zip-full", "Status: install ok installed"))
+        res.append(ssh_checkout(self.data.get("ip"), self.data.get("user"), self.data.get("passwd"),
+                                f"echo {self.data.get('passwd')} | sudo -S dpkg -i {self.data.get('remote_path')}", "Настраивается пакет"))
+        res.append(ssh_checkout(self.data.get("ip"), self.data.get(""), self.data.get("passwd"),
+                                f"echo {self.data.get('passwd')} | sudo -S dpkg -s p7zip-full", "Status: install ok installed"))
 
         return all(res)
 
@@ -28,10 +32,12 @@ class SSHTest:
         res.append(ssh_checkout(self.data.get("invalid_ip"), self.data.get("user"), self.data.get("passwd"), "echo Connected to SSH"))
 
         # Загрузка файлов по SSH
-        res.append(upload_files(self.data.get("ip"), self.data.get("user"), self.data.get("passwd"), "non_existent_file.txt", self.data.get("remote_path")))
+        res.append(upload_files(self.data.get("ip"), self.data.get("user"), self.data.get("passwd"),
+                                "non_existent_file.txt", self.data.get("remote_path")))
 
         # Выполнение команды по SSH и проверка результатов
-        res.append(ssh_checkout(self.data.get("ip"), self.data.get("user"), self.data.get("passwd"), "invalid_command", "Expected output"))
+        res.append(ssh_checkout(self.data.get("ip"), self.data.get("user"), self.data.get("passwd"),
+                                "invalid_command", "Expected output"))
 
         return all(res)
 
